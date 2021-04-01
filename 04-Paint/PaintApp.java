4def1656aae7429b227e23cd889284725e283bc3
Douglas Milanez <dgmilanez@gmail.com>
2021-04-01 12:13:59 -0300

    Resposta do exercício 3.1.

--- 04-Paint/PaintApp.java
+++ 04-Paint/PaintApp.java
@@ -39,6 +39,42 @@ class Rect {
 	}
 }
 
+class Ellipse {
+	int x,y;
+	int w,h;
+	Color fundo,contorno;
+	
+	Ellipse (int x, int y, int w, int h, Color fundo, Color contorno){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.fundo = fundo;
+		this.contorno = contorno;
+	}
+	
+	void print(){
+		System.out.format("(%d,%d) / (%d,%d)\n",
+				this.w,this.h,this.x,this.y);
+	}
+
+	void paint(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setColor(this.fundo);
+		g2d.fill(new Ellipse2D.Double(
+				this.x,this.y,
+				this.w,this.h
+				));
+
+		g2d.setColor(this.contorno);
+		g2d.draw(new Ellipse2D.Double(
+				this.x,this.y,
+				this.w,this.h
+				));
+	}
+}
+
 class PaintApp{
 	public static void main(String[] args){
 		PaintFrame frame = new PaintFrame();
@@ -47,7 +83,7 @@ class PaintApp{
 }
 
 class PaintFrame extends JFrame{
-	Rect r1,r2,r3;
+	Ellipse e1,e2,e3;
 
 	PaintFrame(){
 		this.setTitle("Painting Fogures");
@@ -60,15 +96,15 @@ class PaintFrame extends JFrame{
 		Color FC = new Color(100,255,255);
 		Color CC = new Color(0,0,255);
 
-		this.r1 = new Rect(50,50,100,30,FA,CA);
-		this.r2 = new Rect(200,200,100,50,FB,CB);
-		this.r3 = new Rect(50,200,100,30,FC,CC);
+		this.e1 = new Ellipse(50,50,100,30,FA,CA);
+		this.e2 = new Ellipse(200,200,100,50,FB,CB);
+		this.e3 = new Ellipse(50,200,100,30,FC,CC);
 	}
 
 	public void paint (Graphics g) {
 		super.paint(g);
-		this.r1.paint(g);
-		this.r2.paint(g);
-		this.r3.paint(g);
+		this.e1.paint(g);
+		this.e2.paint(g);
+		this.e3.paint(g);
 	}
 }
